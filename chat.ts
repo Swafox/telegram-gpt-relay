0041f22b4dc64771026205dc706999da43138ac3
Swafox <swafox@pm.me>
2023-04-08 13:01:07 +0300

    Move openai to Deno wrapper

--- chat.ts
+++ chat.ts
@@ -1,17 +1,15 @@
-import { Configuration, OpenAIApi } from "npm:openai";
+import { OpenAI } from "https://deno.land/x/openai@1.3.0/mod.ts";
 import { load } from "https://deno.land/std@0.170.0/dotenv/mod.ts";
 const configData: Record<string, string> = await load();
 const aikey: string = configData["OPENAI_API_KEY"];
 
-const configuration = new Configuration({
-    apiKey: aikey,
-});
-const openai = new OpenAIApi(configuration);
+const openAI = new OpenAI(aikey);
 
+// deno-lint-ignore no-explicit-any
 export default async function chatCompletion(messages: any[]) {
-    const completion = await openai.createChatCompletion({
+    const completion = await openAI.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: messages,
     });
-    return completion.data.choices[0].message?.content;
+    return completion.choices[0].message?.content;
 }
